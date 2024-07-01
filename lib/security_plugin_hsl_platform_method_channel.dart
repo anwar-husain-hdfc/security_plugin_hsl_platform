934e55141ed8b0a2ff6a9f2274ee7bfbdced03eb
prashant.teli <prashant.teli@hdfcsec.com>
2024-07-01 08:58:31 +0530

    play integrity minor correction

--- lib/security_plugin_hsl_platform_method_channel.dart
+++ lib/security_plugin_hsl_platform_method_channel.dart
@@ -167,7 +167,8 @@ class MethodChannelSecurityPluginHslPlatform
   }
 
   bool _isPlayIntegrity(List<String> playIntegrity) {
-    return playIntegrity.contains(MEETS_BASIC_INTEGRITY);
+    return playIntegrity.contains(MEETS_BASIC_INTEGRITY) ||
+        playIntegrity.contains(MEETS_DEVICE_INTEGRITY);
 /*        &&
         playIntegrity.contains(MEETS_DEVICE_INTEGRITY) &&
         playIntegrity.contains(MEETS_STRONG_INTEGRITY);*/
